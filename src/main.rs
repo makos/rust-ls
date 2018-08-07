841289ec4eb052ef64bd92e43fec695879dc133a
Mateusz Makowski <matmakos@gmail.com>
2018-08-07 09:37:23 +0200

    Comment & document functions

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs::DirEntry;
 use std::io;
 use std::path::Path;
 
+// String literals for command-line switches.
 const ALL_FILES_FLAG: &str = "-a";
 const HELP_FLAG: &str = "-h";
 
@@ -14,17 +15,22 @@ struct Options {
 
 impl Options {
     pub fn new(args: &Vec<String>) -> Options {
+        // If no path or switches specified, app defaults to current directory.
         let mut all_files = false;
         let mut help = false;
+        let newline = false;
         let mut path = String::from("./");
 
+        // [1..] to skip the app name which has index 0.
         for arg in &args[1..] {
+            // [..] to pass string slices (&str) instead of String.
             match &arg[..] {
                 ALL_FILES_FLAG => all_files = true,
                 HELP_FLAG => help = true,
-                a @ _ => {
+                // All other arguments to be treated as paths.
+                target_path @ _ => {
                     path.clear();
-                    path = a.to_string();
+                    path = target_path.to_string();
                 }
             }
         }
@@ -41,9 +47,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let opts = Options::new(&args);
     let path = Path::new(&opts.path);
-
+    // Should the output be printed line-by-line (true) or next to each other (false)?
     let mut newline = false;
 
+    // Print help and exit.
     if opts.help {
         print_help(&args[0]);
         return;
@@ -58,10 +65,10 @@ fn main() {
 
         for entry in dirs {
             if let Ok(entry) = entry {
-                // Show all files, even hidden ones
+                // Show all files, even hidden ones.
                 if opts.all_files {
                     parse_metadata_and_print(newline, &entry);
-                // Ignore files starting with a dot
+                // Ignore files starting with a dot.
                 } else {
                     if !entry.file_name().into_string().unwrap().starts_with(".") {
                         parse_metadata_and_print(newline, &entry);
@@ -70,9 +77,36 @@ fn main() {
             }
         }
     }
+    // Add newline after all output is done.
     println!("");
 }
 
+/// Checks if given DirEntry is a directory or a file and passes execution to appropriate printing method.
+/// `newline` specifies whether output should use `println!` instead of `print!`.
+///
+/// # Example
+///
+/// Consider this directory layout:
+/// ````
+/// ./ _
+///    | - .gitignore
+///    | - file.txt
+///    | - somedir/
+/// ````
+///
+/// ````
+/// let newline = false;
+/// let items = std::path::Path::new("./").read_dir().unwrap(); // returns an iterator over DirEntry instances
+/// for item in items {
+///     if let Ok(item) = item {
+///         parse_metadata_and_print(newline, &item);
+///     }
+/// }
+/// ````
+/// Output:
+/// ````
+/// file.txt  somedir/
+/// ````
 fn parse_metadata_and_print(newline: bool, entry: &DirEntry) {
     if let Ok(metadata) = entry.metadata() {
         if metadata.is_dir() {
@@ -83,6 +117,19 @@ fn parse_metadata_and_print(newline: bool, entry: &DirEntry) {
     }
 }
 
+/// Print out directory (filepath with `/` at the end).
+///
+/// # Example
+///
+/// ````
+/// let newline = false;
+/// let dir = "mydir";
+/// print_dir(newline, dir);
+/// ````
+/// Output:
+/// ````
+/// mydir/
+/// ````
 fn print_dir(newline: bool, dir: &str) {
     if newline {
         println!("{}/", dir);
@@ -91,6 +138,19 @@ fn print_dir(newline: bool, dir: &str) {
     }
 }
 
+/// Print out directory (filepath with `/` at the end).
+///
+/// # Example
+///
+/// ````
+/// let newline = false;
+/// let file = "myfile";
+/// print_file(newline, file);
+/// ````
+/// Output:
+/// ````
+/// myfile
+/// ````
 fn print_file(newline: bool, file: &str) {
     if newline {
         println!("{}", file);
@@ -99,6 +159,7 @@ fn print_file(newline: bool, file: &str) {
     }
 }
 
+/// Print out help text.
 fn print_help(name: &String) {
     print!(
         "\
@@ -107,7 +168,8 @@ fn print_help(name: &String) {
 Usage: {n} [OPTIONS]
 Options:
 -h  -- this help
--a  -- show all files\n",
+-a  -- show all files
+",
         n = name
     );
 }
